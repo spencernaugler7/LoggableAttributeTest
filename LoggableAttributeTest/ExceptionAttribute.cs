dec02e0c55c4921d9ad45634e539b19368c1f12f
Spencer Naugler <spencernaugler7@gmail.com>
2025-08-03 16:35:27 -0400

    add null handling

--- LoggableAttributeTest/ExceptionAttribute.cs
+++ LoggableAttributeTest/ExceptionAttribute.cs
@@ -3,6 +3,8 @@ using Metalama.Framework.Aspects;
 
 namespace LoggableAttributeTest;
 
+#pragma warning disable CA2200 // original stack trace is perserved
+
 public class IntecerceptExceptionAttribute : OverrideMethodAspect
 {
     public override dynamic? OverrideMethod()
@@ -11,13 +13,33 @@ public class IntecerceptExceptionAttribute : OverrideMethodAspect
         {
             return meta.Proceed();
         }
-        catch (Exception e) {
+        catch (Exception e)
+        {
             foreach (var param in meta.Target.Method.Parameters)
             {
-                e.Data.Add(param.Name, param.Value.ToString());
+                e.Data.Add(param.Name, param?.Value?.ToString() ?? "null");
             }
 
             throw e;
         }
+    }
+}
+
+public static class EnrichExceptionHandler
+{
+    private const string _slotName = "Context";
+
+    [ExcludeAspect(typeof(EnrichExceptionAttribute))]
+    public static void AppendContextFrame(this Exception e, string frame)
+    {
+
+    }
+}
+
+public class EnrichExceptionAttribute : OverrideMethodAspect
+{
+    public override dynamic? OverrideMethod()
+    {
+        throw new NotImplementedException();
     }
 }
